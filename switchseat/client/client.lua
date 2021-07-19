fa5be3e082341578b9fced529d7f8c3957a5aea2
Dooooc <87667926+Dooooc@users.noreply.github.com>
2021-07-19 20:23:05 +0200

    Add files via upload

---
+++ switchseat/client/client.lua
@@ -0,0 +1,28 @@
+Citizen.CreateThread(function()
+    while true do
+        local plyPed = PlayerPedId()
+        if IsPedSittingInAnyVehicle(plyPed) then
+            local plyVehicle = GetVehiclePedIsIn(plyPed, false)
+			CarSpeed = GetEntitySpeed(plyVehicle) * 3.6 -- On définit la vitesse du véhicule en km/h
+			if CarSpeed <= 40.0 then -- On ne peux pas changer de place si la vitesse du véhicule est au dessus ou égale à 60 km/h
+				if IsControlJustReleased(0, 157) then -- conducteur
+					SetPedIntoVehicle(plyPed, plyVehicle, -1)
+					Citizen.Wait(10)
+				end
+				if IsControlJustReleased(0, 158) then -- avant droit
+					SetPedIntoVehicle(plyPed, plyVehicle, 0)
+					Citizen.Wait(10)
+				end
+				if IsControlJustReleased(0, 160) then -- arriere gauche
+					SetPedIntoVehicle(plyPed, plyVehicle, 1)
+					Citizen.Wait(10)
+				end
+				if IsControlJustReleased(0, 164) then -- arriere gauche
+					SetPedIntoVehicle(plyPed, plyVehicle, 2)
+					Citizen.Wait(10)
+				end
+			end
+		end
+		Citizen.Wait(10) -- Fix Crash Client
+	end
+end)
---
+++ switchseat/fxmanifest.lua
@@ -0,0 +1,6 @@
+version 'v2.5 - Standalone'
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts 'client/client.lua'
